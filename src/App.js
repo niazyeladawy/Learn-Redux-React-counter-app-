62cd850736b457ea54f2954506ea832389e0ee1f
niazyeladawy <niazyadawy2@gmail.com>
2022-05-09 19:47:53 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const globalState = useSelector(state => state);
+
+
+  const counterOperation = (type,payload) => {
+    dispatch({ type: type, payload: payload });
+  }
+
+  
+
+  const toggleCounter = () => {
+    dispatch({ type: "toggleCounter" });
+  }
+
+  const handleCounterValue = (value)=>{
+    if(value < 1){
+      return "smaller than 1";
+    }
+    return value;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Redux Bascis</h1>
+      {
+        globalState.showCounter && 
+        <>
+        <div className='counter'>Counter:{handleCounterValue(globalState.value)}</div>
+        <div>
+          <button onClick={()=>counterOperation("increase",5)}>increase</button>
+          <button onClick={()=>counterOperation("decrease",5)}>decrease</button>
+        </div>
+        </>
+      }
+
+      <div>
+        <button onClick={toggleCounter}>Hide / show counter number</button>
+      </div>
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import { createStore } from "redux";
+
+const initState = {value:8 , showCounter:true};
+
+const counterReducer = (state = initState , action)=>{
+    if(action.type === "increase"){
+        return {...state,value:state.value +action.payload}
+    }
+    if(action.type === "decrease"){
+        return {...state,value:state.value -action.payload}
+    }
+    if(action.type === "toggleCounter"){
+        return {...state,showCounter:!state.showCounter}
+    }
+    return state;
+}
+
+const store = createStore(counterReducer);
+
+export default store;
